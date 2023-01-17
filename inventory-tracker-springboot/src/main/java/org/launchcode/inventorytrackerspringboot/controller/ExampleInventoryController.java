6639598cd6bb28b204822c0780f217ced33bf14a
Suna Ulus <suna.ulus@gmail.com>
2023-01-17 10:08:45 -0600

    added save in controller

--- inventory-tracker-springboot/src/main/java/org/launchcode/inventorytrackerspringboot/controller/ExampleInventoryController.java
+++ inventory-tracker-springboot/src/main/java/org/launchcode/inventorytrackerspringboot/controller/ExampleInventoryController.java
@@ -3,10 +3,7 @@ package org.launchcode.inventorytrackerspringboot.controller;
 import org.launchcode.inventorytrackerspringboot.model.ExampleInventory;
 import org.launchcode.inventorytrackerspringboot.repository.ExampleInventoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,4 +18,15 @@ public class ExampleInventoryController {
     public List<ExampleInventory> getAllInventories(){
         return exampleInventoryRepository.findAll();
     }
+
+    @PostMapping("inventories")
+    void addExampleInventory(@RequestBody ExampleInventory exampleInventory) {
+        ExampleInventory item= new ExampleInventory(item.getId(),
+                item.getExItemName(),
+                item.getExCategory());
+        exampleInventoryRepository.save(item);
+    }
+
+
+
 }
